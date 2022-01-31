b88bc15efb2b6d5bb058c50241673dfbac61a660
sarahob96 <sarahobrien15@gmail.com>
2022-01-31 20:09:40 +0000

    add the questions and answers to the harry potter js file

---
+++ assets/js/harry-potter.js
@@ -0,0 +1,85 @@
+
+let harryPotterQuestions = [
+{
+    question: "Harrys uncle and aunt, the Dursleys, live at what address?",
+    a: "Number 4, Privet Drive",
+    b: "Nine and three-quarters",
+    c: "The trolls club",
+    d: "Gringotts",
+    answer: "a"
+},
+{
+    question: "Harry has a scar on his forehead. What shape is it?",
+    a: "An egg",
+    b: "A lightning bolt",
+    c: "A pigs tail",
+    d: "A shining star",
+    answer: "b"
+
+},
+{
+    question: "Hermiones Parents are not wizards. What non-wizard job do they do?",
+    a: "Physician",
+    b: "Tailor",
+    c: "Dentist",
+    d: "Mechanic",
+    answer: "c"
+
+},
+{
+    question: "How do the Dursleys say that Harrys parents died?",
+    a: "In a car crash",
+    b: "In an aeroplane crash",
+    c: "Illness",
+    d: "Heart attack",
+    answer: "a"
+},
+{
+    question: "What age was Voldemort when he opened the chamber of secrets?",
+    a: "15 years",
+    b: "16 years",
+    c: "17 years",
+    d: "18 years",
+    answer: "b"
+},
+{
+    question: "On a Quidditch pitch, how many goal posts are there in total?",
+    a: "Four",
+    b: "Five",
+    c: "Six",
+    d: "Seven",
+    answer: "c"
+},
+{
+    question: "What does Hagrid name his baby dragon?",
+    a: "Fluffy",
+    b: "Hedwig",
+    c: "Baby dragon",
+    d: "Norbert",
+    answer: "d"
+},
+{
+    question: "What is the term for non-magical people?",
+    a: "Dentists",
+    b: "Muggles",
+    c: "Wizards",
+    d: "Hogwarts",
+    answer: "b"
+},
+{
+    question: "Which is not among the four position on a Quidditch team?",
+    a: "Chaser",
+    b: "Seeker",
+    c: "Keeper",
+    d: "Goalkeeper",
+    answer: "d"
+},
+{
+    question: "Which spell can levitate objects?",
+    a: "Wand",
+    b:"Petrificius Totalu",
+    c: "Alohomora",
+    d: "Wingardium Leviosa",
+    answer: "d"
+}
+]
