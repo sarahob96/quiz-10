fe3dbf11508cc82e9fb625e4f44909b4458edcf5
sarahob96 <sarahobrien15@gmail.com>
2022-02-01 19:49:21 +0000

    add the quiz questions for the antarctica quiz in the js quiz file

---
+++ assets/js/antarctica.js
@@ -0,0 +1,119 @@
+const start= document.getElementsByClassName("start-btn");
+const startPage= document.getElementsByClassName("start-page");
+const rulesPage= document.getElementsByClassName("quiz-rules");
+
+let antarcticaQuestions=[
+    {
+        question: "Which of these animals do you NOT get in Antarctica?",
+        answers:{ 
+            a:"Penguins",
+            b:"Whales",
+            c:"Polar bears",
+            d:"Seals"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "When did humans first SEE Antarctica?",
+        answers:{
+            a: "500 B.C",
+            b: "1000 A.D",
+            c: "1520 A.D",
+            d: "1820 A.D"
+        },
+        correctAnswer: "d"
+    },
+    {
+         question: "All mammals in Antarctica have..",
+         answers:{
+             a: "fur",
+             b: "blubber",
+             c: "fins",
+             d: "teeth"
+         },
+         correctAnswer: "b"
+    },
+    {
+        question: "Antarctica is referred to as..",
+        answers:{
+        a: "The frozen Desert",
+        b: "Research Haven",
+        c: "The Titanic",
+        d: "The iceberg"
+    },
+    correctAnswer: "a"
+},
+{
+    question: "Antarctica is in the..",
+    answers:{
+        a: "Arctic Circle",
+        b: "South Pole",
+        c: "North Pole",
+        d: "Northern Hemisphere"
+    },
+    correctAnswer: "b"
+},
+{
+    question: "How much of the Earths fresh water is found in Antarctica?",
+    answers:{
+        a: "50%",
+        b: "20%",
+        c: "70%",
+        d: "100%"
+    },
+   correctAnswer:"70%"
+},
+{
+    question: "Who are the only people to LIVE in Antarctica (on a temporary basis)?",
+    answers:{
+        a: "Children",
+        b: "Soldiers",
+        c: "Researchers",
+        d: "Politicians"
+    },
+    correctAnswer="c"
+},
+{
+    question: "Which continent is Antarctica closest to?",
+    answers:{
+        a: "North America",
+        b: "Asia",
+        c: "Australia",
+        d: "Europe"
+    },
+    correctAnswer="c"
+},
+{
+    question: "The world's longest glacier is found on Antarctica. What is it called?",
+    answers:{
+        a: "Amundsen",
+        b: "Lambert",
+        c: "Beardmore",
+        d: "Scott"
+    },
+    correctAnswer="b"
+},
+{
+  question: "What research base is located at the South Pole?",
+  answers:{
+      a:"Vostok Research Base",
+      b:"Casey Research Base",
+      c:"Molodezhnaya Research Base",
+      d:"Amundsen-Scott Research Base"
+  },
+  correctAnswers="d"
+}
+]
+
+
+
+
+function buildQuiz{
+ 
+    
+function displayQuestions{
+
+    let output=[];
+    let answers;
+}
+}
